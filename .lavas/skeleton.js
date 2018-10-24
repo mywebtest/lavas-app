b9983e0a98747b1604a2b6b408cbd787a083ae3d
lzj <1875871629@qq.com>
2018-10-24 08:31:01 +0800

    初始化项目

---
+++ .lavas/skeleton.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+
+import Skeleton from 'C:\\Users\\admin\\Desktop\\lavas\\app\\core\\Skeleton.vue';
+
+
+export default new Vue({
+    components: {
+        
+        Skeleton,
+        
+    },
+    template: `
+        <div>
+            
+            <skeleton id="skeleton" style="display:none"/>
+            
+        </div>
+    `
+});
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# lavas-app
-this is a lavas app project
+# app
+
+> 这是一个 Lavas PWA 项目
+
+## npm 相关命令
+
+```bash
+# 安装工程依赖
+npm install
+
+# 在本地启动调试 server
+npm run dev
+
+# 构建线上生产环境产物
+npm run build
+
+# 启动编译后的代码，注意，需要在 dist 目录中启动，仅 SSR 模式下有效
+npm run start
+
+# 检查代码是否符合规范
+npm run lint
+```
